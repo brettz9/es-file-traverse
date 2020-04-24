24731cc6f6a3a159226f12a619fe49d20eb090b1
Brett Zamir <brettz9@yahoo.com>
2020-04-24 21:19:39 +0800

    - Linting: As per latest chai-friendly - npm: Update devDeps

--- test/fixtures/multi-imports.js
+++ test/fixtures/multi-imports.js
@@ -1,8 +1,6 @@
 import './file2.js';
 import './file3.js';
 
-// Todo[eslint-plugin-chai-friendly@>0.5.0]: Remove next line's disabling if this is merged: https://github.com/ihordiachenko/eslint-plugin-chai-friendly/pull/12
-// eslint-disable-next-line chai-friendly/no-unused-expressions
 import('./file4.js');
 
 export {named} from './file5.js';
