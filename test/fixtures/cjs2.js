7970dcacfe18ca0fd698c047283dd62a618e47ea
Brett Zamir <brettz9@yahoo.com>
2020-04-28 11:14:41 +0800

    - Testing: Check JSON

--- test/fixtures/cjs2.js
+++ test/fixtures/cjs2.js
@@ -6,4 +6,7 @@ require('./cjs3.js');
 // Check builtins, ignored by default
 require('path');
 
+// Check JSON file, ignored by default
+require('./package-json-script/package.json');
+
 module.exports = {};
